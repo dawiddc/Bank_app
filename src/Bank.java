aaffaf8517d5340ea61f635ccb808c5cdf203a4a
dawiddc <dawid.ciok@gmail.com>
2018-03-14 16:01:05 +0100

    Changes in bank and bankAccounts methods

--- src/Bank.java
+++ src/Bank.java
@@ -2,13 +2,34 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Bank {
+    /* Declarations */
     private ArrayList<BankAccount> bankAccounts = new ArrayList<>();
+    private ArrayList<ArrayList<String>> bankHistory = new ArrayList<>();
 
     public ArrayList<BankAccount> getBankAccounts() {
         return bankAccounts;
     }
 
-    public void createBankAccout() {
+    public void createBankAccount() {
         bankAccounts.add(new BankAccount(this));
+    }
+
+    public void removeBankAccount(UUID toBeRemovedId) {
+        BankAccount accountToBeRemoved = findBankAccount(toBeRemovedId);
+        if (accountToBeRemoved != null)
+            bankAccounts.remove(accountToBeRemoved);
+    }
+
+    public BankAccount findBankAccount(UUID id) {
+        for (BankAccount bankAccount : bankAccounts)
+            if (bankAccount.getId().equals(id))
+                return bankAccount;
+        return null;
+    }
+
+    public String getReport() {
+        String report = "Couldn't find information...";
+
+        return report;
     }
 }
--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,15 +1,25 @@
+import java.util.ArrayList;
 import java.util.UUID;
+import java.util.Date;
 
 public class BankAccount extends Product {
-    private UUID id;
+    /* Declarations */
+    final private UUID id;
     private double balance;
     private Bank bank;
 
+    /* Getters and setters */
+    public UUID getId() { return id; }
+    public double getBalance() {return balance; }
+    private ArrayList<String> accountHistory = new ArrayList<>();
+
+    /* Constructor */
     public BankAccount(Bank bank) {
         id = UUID.randomUUID();
         this.bank = bank;
     }
 
+    /* methods */
     public void manageDebet() {
 
     }
@@ -18,15 +28,30 @@ public class BankAccount extends Product {
 
     }
 
-    public void withdrawMoney(Double amount) {
+    public void subtractMoney(Double amount) {
+        logOperation(this.id, -amount);
         balance -= amount;
     }
 
     public void addMoney(Double amount) {
+        logOperation(this.id, amount);
         balance += amount;
     }
 
     public void transferMoney(Double amount, UUID receiverID) {
-        balance -= amount;
+        if ( bank.findBankAccount(receiverID) != null ) {
+            balance -= amount;
+            logOperation(this.id, -amount);
+
+            bank.findBankAccount(receiverID).addMoney(amount);
+            logOperation(this.id, amount);
+        } else {
+            System.out.println("Could not find receiver account");
+        }
+    }
+
+    public void logOperation(UUID id, Double amount) {
+        Date date = new Date();
+        accountHistory.add(date.getTime() + " - " + id + " - " + amount);
     }
 }
